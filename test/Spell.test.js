14d8595e4b83a957fb6c8dd7a00ed8fc9b1faf21
Brian Adams <bpeteradams@gmail.com>
2021-08-28 15:46:02 -0400

    refactored Spell.test.js to make the tests more organized

--- test/Spell.test.js
+++ test/Spell.test.js
@@ -12,32 +12,36 @@ describe('Spell Model', function() {
     expect(newSpell).to.have.a.property('_id');
   })
   describe('Spell Instance Creation', function() {
+   describe('Name Attribute', function() {
     it('Should Return an object with a name attribute when created', function() {
-    const newSpell = new Spell({name: 'Fireball'})
-    expect(newSpell).to.be.an.instanceOf(Spell);
-    expect(newSpell).to.have.a.property('name');
-    expect(newSpell.name).to.be.a('string')
-    expect(newSpell.name).to.equal('Fireball');
+      const newSpell = new Spell({name: 'Fireball'})
+      expect(newSpell).to.be.an.instanceOf(Spell);
+      expect(newSpell).to.have.a.property('name');
+      expect(newSpell.name).to.be.a('string')
+      expect(newSpell.name).to.equal('Fireball');
     })
     it('Should Throw An Error If No Name Is Provided', function() {
-    const newSpell = new Spell({nam: 'Fireball'});
-    newSpell.validate(function(error) {
-      expect(error.errors.name).to.exist
-      expect(error.errors.name.message).to.equal('Please Pass In a Name')
-      })
-    })
-    it('Should Return an object with description attribute ', function() {
-      const newSpell = new Spell({ name: 'Fireball', description: 'A Ball of Fire' });
-      expect(newSpell).to.have.a.property('description');
-      expect(newSpell.description).to.be.a('string');
-      expect(newSpell.description).to.equal('A Ball of Fire');
-    })
-    it('Should Throw a Validation Error If No Description is Provided', function() {
-      const newSpell = new Spell({name: 'Fireball', descr: 'A Ball of Fire'});
-
+      const newSpell = new Spell({nam: 'Fireball'});
       newSpell.validate(function(error) {
-        expect(error.errors.description).to.exist;
-        expect(error.errors.description.message).to.equal('Please Pass In a Description')
+        expect(error.errors.name).to.exist
+        expect(error.errors.name.message).to.equal('Please Pass In a Name')
+        })
+    })
+   })
+    describe('Description Attribute', function() {
+      it('Should Return an object with description attribute ', function() {
+        const newSpell = new Spell({ name: 'Fireball', description: 'A Ball of Fire' });
+        expect(newSpell).to.have.a.property('description');
+        expect(newSpell.description).to.be.a('string');
+        expect(newSpell.description).to.equal('A Ball of Fire');
+      })
+      it('Should Throw a Validation Error If No Description is Provided', function() {
+        const newSpell = new Spell({name: 'Fireball', descr: 'A Ball of Fire'});
+
+        newSpell.validate(function(error) {
+          expect(error.errors.description).to.exist;
+          expect(error.errors.description.message).to.equal('Please Pass In a Description')
+        })
       })
     })
   })
